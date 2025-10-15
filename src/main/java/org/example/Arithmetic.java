c47c3c5f08fd7ffbd685c80b2c5d23b4acde9e2b
thomasHermJr <tjherm01@louisville.edu>
2025-10-15 09:52:45 -0400

    Initial commit

---
+++ src/main/java/org/example/Arithmetic.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+    Template class for arithmetic operations
+    You can modify this class as needed
+*/
+
+public class Arithmetic<T extends Number> {
+    private T firstNumber;
+    private T secondNumber;
+
+    public Arithmetic(T firstNumber, T secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    /*To be continued*/
+
+}
